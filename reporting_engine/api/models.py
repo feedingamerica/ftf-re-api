8a523512f6b52306efee7700961c47860e9f4d0a
anteecy <anteecy@gmail.com>
2021-02-17 20:38:02 -0500

    preparing reports database for api calls

--- reporting_engine/api/models.py
+++ reporting_engine/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Report(models.Model):
+    report_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.report_name
--- reporting_engine/reporting_engine/settings.py
+++ reporting_engine/reporting_engine/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'reporting_engine.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv( 'DB_NAME' ),
-        'USER': os.getenv( 'DB_USER' ),
-        'PASSWORD': os.getenv( 'DB_PW' ), 
-        'HOST': os.getenv( 'DB_HOST' ),
-        'PORT': os.getenv( 'DB_PORT' ),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PW'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
