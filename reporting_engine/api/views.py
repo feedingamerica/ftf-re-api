8b42e29aa3aea3c4ebcb822e8f88bbddfc0fdf01
Drew DeLap <delap.8s@osu.edu>
2021-02-21 18:54:00 -0500

    Preliminary additions to views.py handling a POST request. Added appropriate url parth to link to report_schedule view.

--- reporting_engine/api/views.py
+++ reporting_engine/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+def report_schedule(request):
+    if request.method == 'POST':
+        #serialize input
+        data = request.data
+        #send data to functions to process report schedule
+    else:
+        #to be filled in later
+        return (1)
+    return (1)
