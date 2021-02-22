1e7af3a182cd2d018904205e6439f8ec89398e9c
Josean Martinez <josean@josean.com>
2021-02-22 13:29:45 -0400

    Modified urls and report scheduling view

--- reporting_engine/api/views.py
+++ reporting_engine/api/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
-
+@api_view()
 def report_schedule(request):
     if request.method == 'POST':
         #serialize input
         data = request.data
         #send data to functions to process report schedule
+        return Response({"message": "Got some data for new report schedule!", "data": request.data})
     else:
         #to be filled in later
-        return (1)
-    return (1)
+        return Response({"message": "GET request to report schedule"})
