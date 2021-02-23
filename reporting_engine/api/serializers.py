1d865ddc6cb9e9c597e4ac8a3412282fc741b699
anteecy <anteecy@gmail.com>
2021-02-23 17:45:22 -0500

    updated serialization fields

--- reporting_engine/api/serializers.py
+++ reporting_engine/api/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class ReportSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Report
-        fields = ['report_name']
+        fields = ['report_schedule, start_date, end_date, date_completed']
