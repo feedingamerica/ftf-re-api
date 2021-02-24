dbf86a8b964648277bc99f0df02b123f0040ea8b
Drew DeLap <delap.8s@osu.edu>
2021-02-23 19:26:30 -0500

    Serializer class creation using model serializer and added serializer functionality to views

--- reporting_engine/api/views.py
+++ reporting_engine/api/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import render
 from django.views import View
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.http import HttpResponse, JsonResponse
+from serialize import report_schedule_serializer
+from models import ReportSchedule
 
 # Create your views here.
 
 @api_view(['GET', 'POST'])
 def report_schedule(request):
     if request.method == 'POST':
-        #serialize input
-        data = request.data
-        #determine if report exists
-        #send data to functions to process report schedule
+        #serialize input, within serializer determine if report exists
+        serializer = report_schedule_serializer(data=request.data)
+        #serializer.is_valid() should return true if report does not already exist
+        if serializer.is_valid():
+            serializer.save()
+            #send data to functions to process report schedule
+            return (serializer.data, status = status.HTTP_201_CREATED)
+        return (serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        
         return Response({"message": "Got some data for new report schedule!", "data": request.data})
     else:
         #to be filled in later
