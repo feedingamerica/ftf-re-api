5bc85577f1321ad368fdfa83f457c38869412a48
Patrick Hubbell <hubbell.64@osu.edu>
2021-02-28 20:13:33 -0500

    Move transform layer code to ftf-re-api

--- reporting_engine/reporting_engine/settings.py
+++ reporting_engine/reporting_engine/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'transform_layer',
 ]
 
 MIDDLEWARE = [
