fa7962e6bd135df123bdedc00451d72e82d232f9
abbrugh <abbrugh@gmail.com>
2021-03-04 20:38:55 -0500

    fixed spacing

--- reporting_engine/api/tasks.py
+++ reporting_engine/api/tasks.py
@@ -12,6 +12,7 @@ def periodic_report_generation():
         print(schedule.id)
         print(schedule.date_scheduled)
         
-        # in future, will be calling some functions to actually generate reports# these aren't built yet, but could be something like the below...
+        # in future, will be calling some functions to actually generate reports
+        # these aren't built yet, but could be something like the below...
         # dictionary = get_dictionary(schedule.id)
         # results = call_transformation_layer(dictionary)
