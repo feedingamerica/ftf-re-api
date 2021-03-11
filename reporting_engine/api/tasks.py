b4deb34b9876615d583972d3336120a3a10be061
jaming715 <josean@josean.com>
2021-03-10 20:08:53 -0400

    added calculation dispatcher to tasks.py

--- reporting_engine/api/tasks.py
+++ reporting_engine/api/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
 from api.models import ReportSchedule
+from transform_layer.calculations import CalculationDispatcher
 
 @shared_task
 def test():
@@ -9,10 +10,11 @@ def test():
 def periodic_report_generation():
     for schedule in ReportSchedule.objects.all():
         # make sure this works:
-        print(schedule.id)
-        print(schedule.date_scheduled)
+        print(f'Report Schedule: ID - {schedule.id}, Date Scheduled - {schedule.date_scheduled}')
         
         # in future, will be calling some functions to actually generate reports
         # these aren't built yet, but could be something like the below...
         # dictionary = get_dictionary(schedule.id)
         # results = call_transformation_layer(dictionary)
+        # cd = CalculationDispatcher(dictionary)
+        # results = cd.run_calculations()
