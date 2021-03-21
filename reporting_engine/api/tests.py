cedf90694e2e0f064f55183d72f8d38d8d7b8bb0
jaming715 <josean@josean.com>
2021-03-21 17:21:19 -0400

    Added jeff's changes manually

--- reporting_engine/api/tests.py
+++ reporting_engine/api/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.test import Client
+from api.models import ReportScheduler
+
+c = Client()
+responce = c.post('/api/', {
+                    'run_type': 'onetime',
+                    'timeframe_type' : 'monthly',
+                    'report_scope' : '',
+                    'report_scope_value' : '',
+                    'control_type' : '',
+                    'reporting_dictionary' : '',
+                    'control_age_group_id' : '',
+                    'date_scheduled' : '',
+                    'date_custom_start' : '',
+                    'date_custom_end' : '',
+                    'addin_state_report' : '',
+                    'addin_foodbank_report' :''})
+
 
 # Create your tests here.
