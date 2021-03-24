89cafbdda19abbc7ab52866aeeb7ac96a85a6a7d
Ryan Lannutti <ryan.lannutti@accufibertechnologies.com>
2021-03-24 13:13:37 -0400

    Added control_age_group_id to utility function and made startDate and endDate parameters

--- reporting_engine/api/utils.py
+++ reporting_engine/api/utils.py
@@ -3,7 +3,7 @@ import json
 from django.core.serializers.json import DjangoJSONEncoder
 
 
-def get_data_definitions(report_schedule_id):
+def get_data_definitions(report_schedule_id, startDate, endDate):
     """
     Takes a report schedule id and returns a dictionary/json
     of the necessary data definitions to be used by the
@@ -19,18 +19,14 @@ def get_data_definitions(report_schedule_id):
     report_schedule = ReportSchedule.objects.get(pk=report_schedule_id)
 
     # Get startDate and endDate
-    if report_schedule.run_type.name == "One Time":
-        d["Scope"]["startDate"] = report_schedule.date_custom_start
-        d["Scope"]["endDate"] = report_schedule.date_custom_end
-    elif report_schedule.run_type.name == "Recurring":
-        d["Scope"]["startDate"] = report_schedule.timeframe_type.current_start_date
-        d["Scope"]["endDate"] = report_schedule.timeframe_type.current_end_date
+    d["Scope"]["startDate"] = startDate
+    d["Scope"]["endDate"] = endDate
 
     # Complete all fields in Scope
     d["Scope"]["scope_field"] = report_schedule.report_scope.field_reference
     d["Scope"]["scope_field_value"] = report_schedule.report_scope_value
-    d["Scope"]["control_type_field"] = report_schedule.control_type.name
-    d["Scope"]["control_type_value"] = 1  # For now this will always be 1
+    d["Scope"]["control_type_name"] = report_schedule.control_type.name
+    d["Scope"]["control_age_group_id"] = report_schedule.control_age_group_id
 
     # Add all reports that reference the given ReportSchedule
     # TODO: Modify later for efficiency
