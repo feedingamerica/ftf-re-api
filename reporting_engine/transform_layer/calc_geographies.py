0181194c0c7966a46bd6c5495627bedc10e879bc
Foodshops <32028050+Foodshops@users.noreply.github.com>
2021-04-06 00:19:42 -0400

    data definition 53 made

--- reporting_engine/transform_layer/calc_geographies.py
+++ reporting_engine/transform_layer/calc_geographies.py
@@ -2,12 +2,95 @@ from pandas.core.frame import DataFrame
 import dateutil.parser as parser
 import pandas as pd
 import json
+import statistics
 
 # data def 53
 def get_direction_traveled(data: 'list[DataFrame]'):
-    families = data[1]
-    print(str(families.iloc[0]))
-    pass
+    services = data[0]
+
+    distance_dict = {
+        'NE':list(),
+        'N':list(),
+        'SE':list(),
+        'E':list(),
+        'W':list(),
+        'SW':list(),
+        'NW':list(),
+        'S':list()
+    }
+
+    result_dict = {
+        'NE': {
+            'services':0,
+            'mean_distance':0,
+            'median_distance':0,
+            'min_distance':0,
+            'max_distance':0
+        },
+        'N': {
+            'services':0,
+            'mean_distance':0,
+            'median_distance':0,
+            'min_distance':0,
+            'max_distance':0
+        },
+        'SE': {
+            'services':0,
+            'mean_distance':0,
+            'median_distance':0,
+            'min_distance':0,
+            'max_distance':0
+        },
+        'E': {
+            'services':0,
+            'mean_distance':0,
+            'median_distance':0,
+            'min_distance':0,
+            'max_distance':0
+        },
+        'W': {
+            'services':0,
+            'mean_distance':0,
+            'median_distance':0,
+            'min_distance':0,
+            'max_distance':0
+        },
+        'SW': {
+            'services':0,
+            'mean_distance':0,
+            'median_distance':0,
+            'min_distance':0,
+            'max_distance':0
+        },
+        'NW': {
+            'services':0,
+            'mean_distance':0,
+            'median_distance':0,
+            'min_distance':0,
+            'max_distance':0
+        },
+        'S': {
+            'services':0,
+            'mean_distance':0,
+            'median_distance':0,
+            'min_distance':0,
+            'max_distance':0
+        },
+    }
+
+    for index, row in services.iterrows():
+
+        if row["direction"] != None:
+            result_dict[row["direction"]]["services"]+=1
+            distance_dict[row["direction"]].append(row["distance_miles"])
+
+    for direction in result_dict:
+        result_dict[direction]["mean_distance"] = (sum(distance_dict[direction])/len(distance_dict[direction]))
+        result_dict[direction]["median_distance"] = statistics.median(distance_dict[direction])
+        result_dict[direction]["min_distance"] = min(distance_dict[direction])
+        result_dict[direction]["max_distance"] = max(distance_dict[direction])
+
+    return json.dumps(result_dict)
 
 # data def 54
 def get_windrose(data: 'list[DataFrame]'):
