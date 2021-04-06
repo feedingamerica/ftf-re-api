8899423d8723bd06d200401588e50273e5509136
Foodshops <32028050+Foodshops@users.noreply.github.com>
2021-04-06 00:28:39 -0400

    data def 53 test

--- reporting_engine/transform_layer/tests/test_calc_geographies.py
+++ reporting_engine/transform_layer/tests/test_calc_geographies.py
@@ -38,6 +38,73 @@ BASE_DATA = [base_services, base_families, base_members]
 
 class CalculationsTestCase(unittest.TestCase):
 
+    #test data def 53
+    def test_direction_traveled(self):
+        expected = {
+        'NE': {
+            'services':4434,
+            'mean_distance':6.011814,
+            'median_distance':2.676118,
+            'min_distance':0.265377,
+            'max_distance':95.08928
+        },
+        'N': {
+            'services':3159,
+            'mean_distance':11.899266,
+            'median_distance':6.077369,
+            'min_distance':0.345854,
+            'max_distance':102.40609
+        },
+        'SE': {
+            'services':28775,
+            'mean_distance':7.657749,
+            'median_distance':4.906900,
+            'min_distance':0.345854,
+            'max_distance':125.65186
+        },
+        'E': {
+            'services':24284,
+            'mean_distance':5.852088,
+            'median_distance':5.316541,
+            'min_distance':0.435930,
+            'max_distance':118.44905
+        },
+        'W': {
+            'services':2242,
+            'mean_distance':11.870164,
+            'median_distance':7.312024,
+            'min_distance':0.435941,
+            'max_distance':94.26391
+        },
+        'SW': {
+            'services':19922,
+            'mean_distance':9.566677,
+            'median_distance':9.596235,
+            'min_distance':0.530754,
+            'max_distance':135.71163
+        },
+        'NW': {
+            'services':1650,
+            'mean_distance': 20.327712,
+            'median_distance':20.232065,
+            'min_distance':0.740874,
+            'max_distance':105.66228
+        },
+        'S': {
+            'services':30089,
+            'mean_distance':9.155085,
+            'median_distance':9.139874,
+            'min_distance':0.871847,
+            'max_distance':180.99612
+        },
+        }
+        expected = pandas.DataFrame.from_dict(expected)
+        data = BASE_DATA 
+        func = calc.data_calc_function_switcher[55]
+        result = func(data)
+        resultFrame = pandas.read_json(result)
+        assert_frame_equal(resultFrame, expected, check_like = True)
+
     #test data def 55
     def test_sites_visited_distribution(self):
         expected = {
@@ -51,8 +118,6 @@ class CalculationsTestCase(unittest.TestCase):
         result = func(data)
         resultFrame = pandas.read_json(result)
         assert_frame_equal(resultFrame, expected, check_like = True)
-
-        
 
     #test data def 56
     def test_dummy_trip_coverage(self):
