937560adcb441a742a171263978ed29c8eee0a65
AbidFAli <ali.655@osu.edu>
2021-04-08 10:57:05 -0400

    Copied missing cases in checks for control type from transform-layer-age-groups

--- reporting_engine/transform_layer/services/utils.py
+++ reporting_engine/transform_layer/services/utils.py
@@ -11,5 +11,9 @@ def get_control_query(control_type_name):
         return f"dim_service_types.service_category_code IN (10, 15)"
     elif (control_type_name == "Produce only"):
         return f"dim_service_types.service_category_code IN (20)"
+    elif (control_type_name == "Everything"):
+            return f"dim_service_types.service_category_code = *"
+    elif (control_type_name == "TEFAP"):
+            return f'dim_service_types.name = "TEFAP"'
     else:
         return f"dim_service_types.dummy_is_grocery_service = 1"
