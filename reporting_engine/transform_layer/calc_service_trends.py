bacc74cd938aca64813dac99022bfe96ada420fb
AbidFAli <ali.655@osu.edu>
2021-04-16 18:22:23 -0400

    Fixes for data def 67

--- reporting_engine/transform_layer/calc_service_trends.py
+++ reporting_engine/transform_layer/calc_service_trends.py
@@ -128,14 +128,15 @@ def get_service_summary_hod(data: 'list[DataFrame]'):
 def get_service_summary_dowhod(data:'list[DataFrame]'):
     services = data[0]
     skeleton_dowhod = data[9]
-    skeleton_dowhod.astype({'hour_of_day':'int64'}).dtypes
-
-    trend:DataFrame = skeleton_dowhod.merge(services, how='left', on = 'dayofweek')
-    trend = trend[trend['dummy_time'] == 1]
-    trend = trend.groupby(['dayofweek','daynameofweek','hour_of_day_x','hour_of_day_common','short_time','time_key_start',
-    'is_typical_business_hour'], as_index = False, dropna = False).size()
-    trend = trend.rename(columns={"size": "n_services","hour_of_day_x": "hour_of_day"})
-
+    skeleton_dowhod = skeleton_dowhod.astype({'hour_of_day':'int64'})
+    
+    services = services[['research_service_key','dummy_time', 'dayofweek', 'hour_of_day']]
+    services = services[services['dummy_time'] == 1]
+    services = services.astype({'hour_of_day':'int64'})
+    services = services.groupby(['dayofweek','hour_of_day'], as_index=False, dropna = False).size()
+    services = services.rename(columns={"size": "n_services"})
+    trend = skeleton_dowhod.merge(services, how='left', left_on = ['dayofweek','hour_of_day'], right_on = ['dayofweek','hour_of_day'] )
+    trend = trend.fillna(0)
     return trend.to_json()
 
 # data def 68
