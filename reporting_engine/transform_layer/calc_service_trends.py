ed478116e90f5ab88138a8b3686e9dd166807937
Foodshops <32028050+Foodshops@users.noreply.github.com>
2021-04-15 21:58:41 -0400

    Update calc_service_trends.py

--- reporting_engine/transform_layer/calc_service_trends.py
+++ reporting_engine/transform_layer/calc_service_trends.py
@@ -142,8 +142,9 @@ def get_service_summary_dowhod(data:'list[DataFrame]'):
 def get_service_trend_event(data:'list[DataFrame]'):
     services = data[0]
     skeleton_month = data[3]
+    temp:DataFrame = skeleton_month.merge(pd.DataFrame(services['event_name'].unique()), how='cross')
 
-    trend:DataFrame = skeleton_month.merge(services, how='left', on = 'calendaryearmonth')
+    trend:DataFrame = temp.merge(services, how='left', on = 'calendaryearmonth')
     trend = trend.groupby(['calendaryearmonth','event_name'], as_index = False, dropna = False).size()
     trend = trend.rename(columns={"size": "n_services"})
     return trend.to_json()
