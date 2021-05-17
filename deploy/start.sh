bbb9aaec6d72231e698553253506f1a2c97d6a39
Matt Thompson <thompson2091.portfolio@gmail.com>
2021-05-17 13:09:12 -0400

    updated deployment script to activate virtual environemtn before installing requirements

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,11 +2,13 @@
 
 # install requirements
 cd /home/ubuntu/ftf-re-api
-pip3 install -r requirements.txt
 
 # setup and activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
+
+# install any new requirements
+pip3 install -r requirements.txt
 
 # start apache
 sudo service apache2 start
